9a5096af5a60fa181dc0d0645bf3efb15e930624
adrianmaccia <abmaccia@gmail.com>
2021-03-13 15:15:26 +1100

    completed update

--- Task1C/Program.cs
+++ Task1C/Program.cs
@@ -6,7 +6,20 @@ namespace Task1C
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter a number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter a second number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if(num1 + num2 > 10){
+                Console.Write("Sum of numbers is greater than 10.");
+            }else if(num1+num2 == 10){
+                    Console.Write("Sum of numbers is equal to 10.");
+            }else{
+                Console.Write("Sum of numbers is less than 10.");
+            }
         }
     }
 }
+
